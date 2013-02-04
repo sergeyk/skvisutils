df4b20822f36e01876261ebd6b0954ffc43ac013
Sergey Karayev <sergeykarayev@gmail.com>
2013-02-03 21:01:14 -0800

    iamge done

--- skvisutils/image.py
+++ skvisutils/image.py
@@ -1,182 +1,185 @@
 import os
-import time
 import numpy as np
-from skimage.io import imread,imsave,imshow
+from skimage.io import imread, imshow
 import xml.dom.minidom as minidom
 
 from skpyutils import Table, skutil
 
 from skvisutils import BoundingBox, SlidingWindows
 
+
 class Image(object):
-  """
-  An image has a name, size and a Table of objects.
-  """
-
-  columns = BoundingBox.columns + ['cls_ind', 'diff', 'trun']
-  
-  def __init__(self,width,height,classes=None,name=None):
-    self.width = width
-    self.height = height
-    self.classes = classes
-    self.name = name
-    self.filename = None
-    self.objects_table = None
-    # the above Table is constructed by the loader class
-
-  @property
-  def size(self):
-    return (self.width,self.height)
-
-  def __repr__(self):
-    return "Image (%(name)s)\n  W x H: %(width)d x %(height)d\n  Objects:\n %(objects_table)s" % self.__dict__
-
-  def display_image(self):
     """
-    Display the image using skimage.
+    An image has a name, size and a Table of objects.
     """
-    imshow(self.load_image())
 
-  def load_image(self):
-    """
-    Return the image loaded using skimage.
-    """
-    if not self.filename:
-      print("Image: Cannot load, no associated filename")
-      return None
-    return imread(self.filename)
+    columns = BoundingBox.columns + ['cls_ind', 'diff', 'trun']
 
-  def get_objects(self,with_diff=False,with_trun=True):
-    """
-    Return Table of objects filtered with the parameters.
-    """
-    df = self.objects_table
-    df = df if with_diff else df.filter_on_column('diff',0) 
-    df = df if with_trun else df.filter_on_column('trun',0) 
-    return df
+    def __init__(self, width, height, classes=None, name=None):
+        self.width = width
+        self.height = height
+        self.classes = classes
+        self.name = name
+        self.filename = None
+        self.objects_table = None
+        # the above Table is constructed by the loader method
 
-  def get_det_gt(self, with_diff=False, with_trun=True, cls_name=None):
-    """
-    Return Table of detection ground truth.
-    If class_name is given, only include objects of that class.
-    Filter according to with_diff and with_trun.
-    """
-    objects = self.get_objects(with_diff,with_trun)
-    if cls_name and not cls_name=='all':
-      cls_ind = self.classes.index(cls_name)
-      objects = objects.filter_on_column('cls_ind',cls_ind)
-    return objects
+    @property
+    def size(self):
+        return (self.width, self.height)
 
-  def get_cls_counts(self, with_diff=False, with_trun=True):
-    """
-    Return an array of the counts of each class in the image.
-    """
-    counts = np.zeros(len(self.classes))
-    objects = self.get_objects(with_diff,with_trun)
-    if objects.shape[0]>0:
-      cls_inds = objects.subset_arr('cls_ind').astype('int')
-      bincount = np.bincount(cls_inds)
-      # need to pad this with zeros for total length of num_classes
-      counts[:bincount.size] = bincount
-    return counts
+    def __repr__(self):
+        return "Image (%(name)s)\n    W x H: %(width)d x %(height)d\n    Objects:\n %(objects_table)s" % self.__dict__
 
-  def get_cls_gt(self,with_diff=False,with_trun=False):
-    """
-    Return an array of class presence (True/False) ground truth.
-    """
-    return self.get_cls_counts(with_diff,with_trun)>0
+    def display_image(self):
+        """
+        Display the image using skimage.
+        """
+        imshow(self.load_image())
 
-  def contains_class(self, cls_name, with_diff=False, with_trun=True):
-    """
-    Return whether the image contains an object of class cls_name.
-    """
-    gt = self.get_cls_gt(with_diff,with_trun)
-    return gt[self.classes.index(cls_name)]
+    def load_image(self):
+        """
+        Return the image loaded using skimage.
+        """
+        if not self.filename:
+            print("Image: Cannot load, no associated filename")
+            return None
+        return imread(self.filename)
 
-  def contains_cls_ind(self,cls_ind):
-    gt = self.get_cls_gt()
-    return gt[cls_ind]
+    def get_objects(self, with_diff=False, with_trun=True):
+        """
+        Return Table of objects filtered with the parameters.
+        """
+        df = self.objects_table
+        df = df if with_diff else df.filter_on_column('diff', 0)
+        df = df if with_trun else df.filter_on_column('trun', 0)
+        return df
 
-  ### 
-  # Windows
-  ###
-  def get_whole_image_bbox(self):
-    "Return a BoundingBox with (0,0,width,height) of the image."
-    return BoundingBox((0,0,self.width,self.height))
+    def get_det_gt(self, with_diff=False, with_trun=True, cls_name=None):
+        """
+        Return Table of detection ground truth.
+        If class_name is given, only include objects of that class.
+        Filter according to with_diff and with_trun.
+        """
+        objects = self.get_objects(with_diff, with_trun)
+        if cls_name and not cls_name == 'all':
+            cls_ind = self.classes.index(cls_name)
+            objects = objects.filter_on_column('cls_ind', cls_ind)
+        return objects
 
-  def get_windows(self,window_params,with_time=False):
-    "Return all windows that can be generated with given params."
-    return SlidingWindows.get_windows(self,None,window_params,with_time)
+    def get_cls_counts(self, with_diff=False, with_trun=True):
+        """
+        Return an array of the counts of each class in the image.
+        """
+        counts = np.zeros(len(self.classes))
+        objects = self.get_objects(with_diff, with_trun)
+        if objects.shape[0] > 0:
+            cls_inds = objects.subset_arr('cls_ind').astype('int')
+            bincount = np.bincount(cls_inds)
+            # need to pad this with zeros for total length of num_classes
+            counts[:bincount.size] = bincount
+        return counts
 
-  def get_random_windows(self,window_params,num_windows):
-    "Return at most num_windows random windows generated according to params."
-    windows = self.get_windows(window_params)
-    return windows[skutil.random_subset_up_to_N(windows.shape[0],num_windows),:]
+    def get_cls_gt(self, with_diff=False, with_trun=False):
+        """
+        Return an array of class presence (True/False) ground truth.
+        """
+        return self.get_cls_counts(with_diff, with_trun) > 0
 
-  ###
-  # Loaders
-  ###
-  @classmethod
-  def load_from_json(cls, classes, data):
-    "Return an Image instantiated from a JSON representation."
-    name = data['name']
-    width = data['size'][0]
-    height = data['size'][1]
-    img = Image(width,height,classes,name)
-    objects = []
-    for obj in data['objects']:
-      bbox = BoundingBox(obj['bbox'])
-      cls_name = obj['class']
-      cls_ind = classes.index(cls_name)
-      diff = obj['diff']
-      trun = obj['trun']
-      objects.append(np.hstack((bbox.get_arr(), cls_ind, diff, trun)))
-    if len(objects)>0:
-      img.objects_table = Table(np.array(objects), cls.columns)
-    else:
-      img.objects_table = Table(None, cls.columns)
-    return img
-  
-  @classmethod
-  def load_from_pascal_xml_filename(cls, classes, filename, images_dir):
-    "Load image info from a file in the PASCAL VOC XML format."
+    def contains_class(self, cls_name, with_diff=False, with_trun=True):
+        """
+        Return whether the image contains an object of class cls_name.
+        """
+        gt = self.get_cls_gt(with_diff, with_trun)
+        return gt[self.classes.index(cls_name)]
 
-    def get_data_from_tag(node, tag):
-      if tag is "bndbox":
-        x1 = int(node.getElementsByTagName(tag)[0].childNodes[1].childNodes[0].data)
-        y1 = int(node.getElementsByTagName(tag)[0].childNodes[3].childNodes[0].data)
-        x2 = int(node.getElementsByTagName(tag)[0].childNodes[5].childNodes[0].data)
-        y2 = int(node.getElementsByTagName(tag)[0].childNodes[7].childNodes[0].data)
-        return (x1, y1, x2, y2)
-      else:
-        return node.getElementsByTagName(tag)[0].childNodes[0].data
+    def contains_cls_ind(self, cls_ind):
+        gt = self.get_cls_gt()
+        return gt[cls_ind]
 
-    with open(filename) as f:
-      data = minidom.parseString(f.read())
+    ###
+    # Windows
+    ###
+    def get_whole_image_bbox(self):
+        "Return a BoundingBox with (0,0,width,height) of the image."
+        return BoundingBox((0, 0, self.width, self.height))
 
-    # image info
-    name = get_data_from_tag(data, "filename")
-    filename = os.path.join(images_dir, name)
-    size = data.getElementsByTagName("size")[0]
-    im_width = int(get_data_from_tag(size, "width"))
-    im_height = int(get_data_from_tag(size, "height"))
-    im_depth = int(get_data_from_tag(size, "depth"))
-    width = im_width
-    height = im_height
-    img = Image(width,height,classes,name)
+    def get_windows(self, window_params, with_time=False):
+        "Return all windows that can be generated with given params."
+        return SlidingWindows.get_windows(self, None, window_params, with_time)
 
-    # per-object info
-    objects = []
-    for obj in data.getElementsByTagName("object"):
-      clas = str(get_data_from_tag(obj, "name")).lower().strip()
-      diff = int(get_data_from_tag(obj, "difficult"))
-      trun = int(get_data_from_tag(obj, "truncated"))
-      rect = get_data_from_tag(obj, "bndbox")
-      bbox = BoundingBox(rect, format='corners')
-      cls_ind = classes.index(clas)
-      objects.append(np.hstack((bbox.get_arr(), cls_ind, diff, trun)))
-    if len(objects)>0:
-      img.objects_table = Table(np.array(objects), cls.columns)
-    else:
-      img.objects_table = Table(None, cls.columns)
-    return img
+    def get_random_windows(self, window_params, num_windows):
+        "Return at most num_windows random windows generated according to params."
+        windows = self.get_windows(window_params)
+        return windows[skutil.random_subset_up_to_N(windows.shape[0], num_windows), :]
+
+    ###
+    # Loaders
+    ###
+    @classmethod
+    def load_from_json(cls, classes, data):
+        "Return an Image instantiated from a JSON representation."
+        name = data['name']
+        width = data['size'][0]
+        height = data['size'][1]
+        img = Image(width, height, classes, name)
+        objects = []
+        for obj in data['objects']:
+            bbox = BoundingBox(obj['bbox'])
+            cls_name = obj['class']
+            cls_ind = classes.index(cls_name)
+            diff = obj['diff']
+            trun = obj['trun']
+            objects.append(np.hstack((bbox.get_arr(), cls_ind, diff, trun)))
+        if len(objects) > 0:
+            img.objects_table = Table(np.array(objects), cls.columns)
+        else:
+            img.objects_table = Table(None, cls.columns)
+        return img
+
+    @classmethod
+    def load_from_pascal_xml_filename(cls, classes, filename, images_dir):
+        "Load image info from a file in the PASCAL VOC XML format."
+
+        def get_data_from_tag(node, tag):
+            if tag is "bndbox":
+                x1 = int(node.getElementsByTagName(
+                    tag)[0].childNodes[1].childNodes[0].data)
+                y1 = int(node.getElementsByTagName(
+                    tag)[0].childNodes[3].childNodes[0].data)
+                x2 = int(node.getElementsByTagName(
+                    tag)[0].childNodes[5].childNodes[0].data)
+                y2 = int(node.getElementsByTagName(
+                    tag)[0].childNodes[7].childNodes[0].data)
+                return (x1, y1, x2, y2)
+            else:
+                return node.getElementsByTagName(tag)[0].childNodes[0].data
+
+        with open(filename) as f:
+            data = minidom.parseString(f.read())
+
+        # image info
+        name = get_data_from_tag(data, "filename")
+        filename = os.path.join(images_dir, name)
+        size = data.getElementsByTagName("size")[0]
+        im_width = int(get_data_from_tag(size, "width"))
+        im_height = int(get_data_from_tag(size, "height"))
+        width = im_width
+        height = im_height
+        img = Image(width, height, classes, name)
+
+        # per-object info
+        objects = []
+        for obj in data.getElementsByTagName("object"):
+            clas = str(get_data_from_tag(obj, "name")).lower().strip()
+            diff = int(get_data_from_tag(obj, "difficult"))
+            trun = int(get_data_from_tag(obj, "truncated"))
+            rect = get_data_from_tag(obj, "bndbox")
+            bbox = BoundingBox(rect, format='corners')
+            cls_ind = classes.index(clas)
+            objects.append(np.hstack((bbox.get_arr(), cls_ind, diff, trun)))
+        if len(objects) > 0:
+            img.objects_table = Table(np.array(objects), cls.columns)
+        else:
+            img.objects_table = Table(None, cls.columns)
+        return img
--- test/image.py
+++ test/image.py
@@ -1,135 +1,141 @@
 from context import *
 import time
-from skpyutils import Table, skutil
-
+from skpyutils import Table
 from skvisutils import Image, BoundingBox, WindowParams
 
+
 class TestImage(object):
-  def setup(self):
-    self.data = {
-      "name": "test_image",
-      "size": [640,480],
-      "objects": [
-        {"class":"A", "bbox": [0,0,0,0], "diff": 0, "trun": 0},
-        {"class":"B", "bbox": [1,1,1,1], "diff": 0, "trun": 0},
-        {"class":"C", "bbox": [2,2,2,2], "diff": 0, "trun": 0}
-      ]
-    }
-    self.classes = ['A','B','C']
-    self.columns = BoundingBox.columns+['cls_ind','diff','trun']
+    def setup(self):
+        self.data = {
+            "name": "test_image",
+            "size": [640, 480],
+            "objects": [
+                {"class": "A", "bbox": [0, 0, 0, 0], "diff": 0, "trun": 0},
+                {"class": "B", "bbox": [1, 1, 1, 1], "diff": 0, "trun": 0},
+                {"class": "C", "bbox": [2, 2, 2, 2], "diff": 0, "trun": 0}
+            ]
+        }
+        self.classes = ['A', 'B', 'C']
+        self.columns = BoundingBox.columns + ['cls_ind', 'diff', 'trun']
 
-  def test_constructor(self):
-    image = Image(20,10,['A','B','C'],'test_image')
-    assert(image.width == 20 and image.height == 10)
-    assert(image.classes == ['A','B','C'])
-    assert(image.name == 'test_image')
+    def test_constructor(self):
+        image = Image(20, 10, ['A', 'B', 'C'], 'test_image')
+        assert(image.width == 20 and image.height == 10)
+        assert(image.classes == ['A', 'B', 'C'])
+        assert(image.name == 'test_image')
 
-  def test_get_whole_image_bbox(self):
-    image = Image(20,10,[],'test_image')
-    assert(image.get_whole_image_bbox() == BoundingBox((0,0,20,10)))
-    image = Image(2,100,[],'test_image')
-    assert(image.get_whole_image_bbox() == BoundingBox((0,0,2,100)))
+    def test_get_whole_image_bbox(self):
+        image = Image(20, 10, [], 'test_image')
+        assert(image.get_whole_image_bbox() == BoundingBox((0, 0, 20, 10)))
+        image = Image(2, 100, [], 'test_image')
+        assert(image.get_whole_image_bbox() == BoundingBox((0, 0, 2, 100)))
 
-  def test_load_json_data(self):
-    image = Image.load_from_json(self.classes,self.data)
-    assert(image.width == 640 and image.height == 480)
-    assert(image.classes == ['A','B','C'])
-    assert(image.name == 'test_image')
-    objects_table = Table(np.array([
-      [0,0,0,0,0,0,0],
-      [1,1,1,1,1,0,0],
-      [2,2,2,2,2,0,0]]), self.columns)
-    assert(image.objects_table == objects_table)
+    def test_load_json_data(self):
+        image = Image.load_from_json(self.classes, self.data)
+        assert(image.width == 640 and image.height == 480)
+        assert(image.classes == ['A', 'B', 'C'])
+        assert(image.name == 'test_image')
+        objects_table = Table(np.array([
+            [0, 0, 0, 0, 0, 0, 0],
+            [1, 1, 1, 1, 1, 0, 0],
+            [2, 2, 2, 2, 2, 0, 0]]), self.columns)
+        assert(image.objects_table == objects_table)
 
-  def test_get_det_gt(self):
-    image = Image.load_from_json(self.classes,self.data)
-    objects_table = Table(np.array([
-      [0,0,0,0,0,0,0],
-      [1,1,1,1,1,0,0],
-      [2,2,2,2,2,0,0]]), self.columns)
-    assert(image.get_objects() == objects_table)
+    def test_get_det_gt(self):
+        image = Image.load_from_json(self.classes, self.data)
+        objects_table = Table(np.array([
+            [0, 0, 0, 0, 0, 0, 0],
+            [1, 1, 1, 1, 1, 0, 0],
+            [2, 2, 2, 2, 2, 0, 0]]), self.columns)
+        assert(image.get_objects() == objects_table)
 
-    data = self.data.copy()
-    data['objects'][0]['diff'] = 1
-    data['objects'][1]['trun'] = 1
-    image = Image.load_from_json(self.classes,data)
-    objects_table = Table(np.array([
-      [0,0,0,0,0,1,0],
-      [1,1,1,1,1,0,1],
-      [2,2,2,2,2,0,0]]), self.columns)
-    assert(image.get_objects(with_diff=True,with_trun=True) == objects_table)
+        data = self.data.copy()
+        data['objects'][0]['diff'] = 1
+        data['objects'][1]['trun'] = 1
+        image = Image.load_from_json(self.classes, data)
+        objects_table = Table(np.array([
+            [0, 0, 0, 0, 0, 1, 0],
+            [1, 1, 1, 1, 1, 0, 1],
+            [2, 2, 2, 2, 2, 0, 0]]), self.columns)
+        assert(image.get_objects(
+            with_diff=True, with_trun=True) == objects_table)
 
-    objects_table = Table(np.array([
-      [1,1,1,1,1,0,1],
-      [2,2,2,2,2,0,0]]),self.columns)
-    assert(image.get_objects(with_diff=False,with_trun=True) == objects_table)
+        objects_table = Table(np.array([
+            [1, 1, 1, 1, 1, 0, 1],
+            [2, 2, 2, 2, 2, 0, 0]]), self.columns)
+        assert(image.get_objects(
+            with_diff=False, with_trun=True) == objects_table)
 
-    # this should be default behavior
-    assert(image.get_objects() == objects_table)
+        # this should be default behavior
+        assert(image.get_objects() == objects_table)
 
-    objects_table = Table(np.array([
-      [2,2,2,2,2,0,0]]),self.columns)
-    assert(image.get_objects(with_diff=False,with_trun=False) == objects_table)
+        objects_table = Table(np.array([
+            [2, 2, 2, 2, 2, 0, 0]]), self.columns)
+        assert(image.get_objects(
+            with_diff=False, with_trun=False) == objects_table)
 
-    objects_table = Table(np.array([
-      [0,0,0,0,0,1,0],
-      [2,2,2,2,2,0,0]]), self.columns)
-    assert(image.get_objects(with_diff=True,with_trun=False) == objects_table)
+        objects_table = Table(np.array([
+            [0, 0, 0, 0, 0, 1, 0],
+            [2, 2, 2, 2, 2, 0, 0]]), self.columns)
+        assert(image.get_objects(
+            with_diff=True, with_trun=False) == objects_table)
 
-    # What if everything is filtered out?
-    data['objects'] = data['objects'][:-1]
-    objects_table = Table(np.array([
-      [0,0,0,0,0,1,0],
-      [1,1,1,1,1,0,1]]), self.columns)
-    image = Image.load_from_json(self.classes,data)
-    assert(image.get_objects(with_diff=True,with_trun=True) == objects_table)
-    assert(image.get_objects(with_diff=False,with_trun=False).shape[0] == 0)
+        # What if everything is filtered out?
+        data['objects'] = data['objects'][:-1]
+        objects_table = Table(np.array([
+            [0, 0, 0, 0, 0, 1, 0],
+            [1, 1, 1, 1, 1, 0, 1]]), self.columns)
+        image = Image.load_from_json(self.classes, data)
+        assert(image.get_objects(
+            with_diff=True, with_trun=True) == objects_table)
+        assert(
+            image.get_objects(with_diff=False, with_trun=False).shape[0] == 0)
 
-  def test_get_cls_counts_and_gt(self):
-    data = self.data.copy()
-    image = Image.load_from_json(self.classes,data)
-    assert(np.all(image.get_cls_counts() == np.array([1,1,1])))
-    assert(np.all(image.get_cls_gt() == np.array([True,True,True])))
-    assert(image.contains_class('A') == True)
-    assert(image.contains_class('B') == True)
+    def test_get_cls_counts_and_gt(self):
+        data = self.data.copy()
+        image = Image.load_from_json(self.classes, data)
+        assert(np.all(image.get_cls_counts() == np.array([1, 1, 1])))
+        assert(np.all(image.get_cls_gt() == np.array([True, True, True])))
+        assert(image.contains_class('A'))
+        assert(image.contains_class('B'))
 
-    data['objects'][0]['class'] = 'B'
-    image = Image.load_from_json(self.classes,data)
-    # doesn't actually have to be Series, can be ndarray for comparison
-    assert(np.all(image.get_cls_counts() == np.array([0,2,1])))
-    assert(np.all(image.get_cls_gt() == np.array([False,True,True])))
-    assert(image.contains_class('A') == False)
-    assert(image.contains_class('B') == True)
+        data['objects'][0]['class'] = 'B'
+        image = Image.load_from_json(self.classes, data)
+        # doesn't actually have to be Series, can be ndarray for comparison
+        assert(np.all(image.get_cls_counts() == np.array([0, 2, 1])))
+        assert(np.all(image.get_cls_gt() == np.array([False, True, True])))
+        assert(not image.contains_class('A'))
+        assert(image.contains_class('B'))
 
-    data['objects'] = []
-    image = Image.load_from_json(self.classes,data)
-    assert(np.all(image.get_cls_counts() == np.array([0,0,0])))
-    assert(np.all(image.get_cls_gt() == np.array([False,False,False])))
-    assert(image.contains_class('A') == False)
-    assert(image.contains_class('B') == False)
+        data['objects'] = []
+        image = Image.load_from_json(self.classes, data)
+        assert(np.all(image.get_cls_counts() == np.array([0, 0, 0])))
+        assert(np.all(image.get_cls_gt() == np.array([False, False, False])))
+        assert(not image.contains_class('A'))
+        assert(not image.contains_class('B'))
 
-  def test_get_random_windows(self):
-    image = Image(width=3,height=2,classes=[],name='test')
-    window_params = WindowParams(
-        min_width=2,stride=1,scales=[1,0.5],aspect_ratios=[0.5])
-    windows = image.get_random_windows(window_params,2)
-    assert(windows.shape[0] == 2)
-    windows = image.get_random_windows(window_params,3)
-    assert(windows.shape[0] == 3)
+    def test_get_random_windows(self):
+        image = Image(width=3, height=2, classes=[], name='test')
+        window_params = WindowParams(
+            min_width=2, stride=1, scales=[1, 0.5], aspect_ratios=[0.5])
+        windows = image.get_random_windows(window_params, 2)
+        assert(windows.shape[0] == 2)
+        windows = image.get_random_windows(window_params, 3)
+        assert(windows.shape[0] == 3)
 
-  def test_get_windows_lots(self):
-    t = time.time()
-    image = Image(width=640,height=480,classes=[],name='test')
-    window_params = WindowParams()
-    window_params.min_width=10
-    window_params.stride=8
-    window_params.aspect_ratios=[0.5,1,1.5]
-    window_params.scales=1./2**np.array([0,0.5,1,1.5,2])
-    print(window_params)
-    windows = image.get_windows(window_params)
-    time_passed = time.time()-t
-    print("Generating windows took %.3f seconds"%time_passed)
-    print(np.shape(windows))
-    print(windows[:10,:])
-    rand_ind = np.random.permutation(np.shape(windows)[0])[:10]
-    print(windows[rand_ind,:])
+    def test_get_windows_lots(self):
+        t = time.time()
+        image = Image(width=640, height=480, classes=[], name='test')
+        window_params = WindowParams()
+        window_params.min_width = 10
+        window_params.stride = 8
+        window_params.aspect_ratios = [0.5, 1, 1.5]
+        window_params.scales = 1. / 2 ** np.array([0, 0.5, 1, 1.5, 2])
+        print(window_params)
+        windows = image.get_windows(window_params)
+        time_passed = time.time() - t
+        print("Generating windows took %.3f seconds" % time_passed)
+        print(np.shape(windows))
+        print(windows[:10, :])
+        rand_ind = np.random.permutation(np.shape(windows)[0])[:10]
+        print(windows[rand_ind, :])
